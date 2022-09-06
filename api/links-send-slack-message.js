8204b56db0ccc5e919ba96c23d9b8f012a7cce2a
Valeriu Timbuc <valeriu@vtimbuc.dev>
2022-09-06 21:32:11 +0300

    update links-send-slack-message.js

--- api/links-send-slack-message.js
+++ api/links-send-slack-message.js
@@ -1,15 +1,19 @@
 import fetch from 'node-fetch'
 
 export default async function handler(request, response) {
-  console.log({ a: request.headers })
+  const country = request.headers['x-vercel-ip-country']
+  const city = request.headers['x-vercel-ip-city']
+  const ip = request.headers['x-vercel-forwarded-for']
 
   await fetch(process.env.LINKS_SLACK_WEBHOOK_URL, {
     method: 'POST',
     body: JSON.stringify({
-      text: 'Test'
+      text: `*Country:* ${country || '-'}\n*City:* ${city || '-'}\n*IP:* ${
+        ip || '-'
+      }`
     }),
     headers: { 'Content-Type': 'application/json' }
   })
 
-  return response.status(200)
+  return response.status(200).end()
 }
